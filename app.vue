925cad56c88877f1a15ffa2433cf6f4e0023dfec
Marius Andresen <marius.e.g.a@gmail.com>
2022-10-08 23:54:24 +0200

    test

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 v-for="article in data">{{ article.title }}</h1>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref, computed } from 'vue'
+const articles = ref([])
+
+const { data, pending, error, refresh } = await useAsyncData(  'articles',  () => $fetch('https://notion-api.splitbee.io/v1/table/What-is-Nuxt-Getting-started-with-create-nuxt-app-309e15c224034b099ee041e5b82e1e47'))
+</script>
